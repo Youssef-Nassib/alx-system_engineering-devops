c5a49b13d5005cd026a17c0e52b01864ac7bbebc
Youssef <wissal2015.nassib@gmail.com>
2024-08-13 15:57:09 +0000

    How many subs?

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,23 +1,16 @@
-#!/bin/usr/python3
+#!/bin/usr/pythone3
 """function that queries the Reddit API and returns the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
-    """methode"""
-    url = f"https://www.reddit.com/r/{subreddit}/about.json"
-    
-    # Set a custom User-Agent to avoid rate limiting issues
-    headers = {'User-Agent': 'my-app'}
-    
-    try:
-
-        response = requests.get(url, headers=headers, allow_redirects=False)
-        
-
-        if response.status_code == 200:
-            data = response.json()
-            return data['data']['subscribers']
-        else:
-            return 0
-    except Exception:
+    """Return the total number of subscribers on a given subreddit."""
+    url = "https://www.reddit.com/r/{}/about.json".format(subreddit)
+    headers = {"User-Agent": "Mozilla/5.0"}
+    response = requests.get(url, headers=headers, allow_redirects=False)
+    if response.status_code == 200:
+        data = response.json()
+        subscribers = data['data']['subscribers']
+        return subscribers
+    else:
         return 0
